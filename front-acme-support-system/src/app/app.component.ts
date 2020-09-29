5c6a61ab0fc585cc165ad34c257f51d5b408618e
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2020-09-28 22:46:36 -0300

    confirando prime-ng

--- front-acme-support-system/src/app/app.component.ts
+++ front-acme-support-system/src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { ToastyConfig } from 'ng2-toasty';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'body',
   template: '<router-outlet></router-outlet><ng2-toasty></ng2-toasty>',
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router, private toastyConfig: ToastyConfig) {
+  constructor(
+    private router: Router,
+    private toastyConfig: ToastyConfig,
+    private primengConfig: PrimeNGConfig
+  ) {
     this.toastyConfig.theme = 'bootstrap';
   }
 
   ngOnInit() {
+    this.primengConfig.ripple = true;
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;
--- front-acme-support-system/src/app/app.module.ts
+++ front-acme-support-system/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Declarations
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,9 @@ import ptBr from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { PanelModule } from 'primeng/panel';
 
 registerLocaleData(ptBr);
 
@@ -20,9 +24,13 @@ registerLocaleData(ptBr);
   declarations: [AppComponent, LoginComponent, DashboardComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     RouterModule,
     FormsModule,
+    InputTextModule,
+    PasswordModule,
+    PanelModule,
     ToastyModule.forRoot(),
   ],
   providers: [

0de55fcd88a4637674cc37655fd3418da615b60b
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2020-10-04 16:42:14 -0300

    Ajustando enviroment do nestjs

--- back-acme-support-system/src/bootstrap.ts
+++ back-acme-support-system/src/bootstrap.ts
@@ -1,10 +1,10 @@
-let path = "../../.env";
+let path = ".env";
 if (process.env.NODE_ENV === "production") {
-  path = "../../.env.production";
+  path = ".env.production";
 } else if (process.env.NODE_ENV === "hml") {
-  path = "../../.env.hml";
+  path = ".env.hml";
 } else if (process.env.NODE_ENV === "test") {
-  path = "../../.env.test";
+  path = ".env.test";
 }
 const { parsed } = require("dotenv").config({ path });
 for (const key in parsed) process.env[key] = parsed[key];
